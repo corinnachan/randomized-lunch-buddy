046acae1297777ccf5845072cd110a3c92cc0702
corinnachan <corinnaccm@gmail.com>
2020-01-06 22:09:34 +0000

    simplified js, linked to html, added json

--- getname.js
+++ getname.js
@@ -1,13 +1,30 @@
+var results = document.querySelector("#name");
+var obj = require('../classname.json');
+
+
 function getName() {
-   var xhr = new XMLHttpRequest;
-   xhr.onreadystatechange = function () {
-       if (xhr.readyState == 4 && xhr.status == 200) {
-           var responseData = JSON.parse(xhr.response);
+   randomNum = Math.floor(Math.random() * Math.floor(332));
+   name = obj[randomNum];
+   
+   results.innerHTML =
+        "Catch up with " + name + "over lunch this week!";
 
-           randomNum = Math.floor(Math.random() * Math.floor(250));
+   
+//     var xhr = new XMLHttpRequest;
+//    xhr.onreadystatechange = function () {
+//        if (xhr.readyState == 4 && xhr.status == 200) {
+//            var responseData = JSON.parse(xhr.response);
 
-           cityName = responseData[randomCountryNum].capital;
-           newCurrency = responseData[randomCountryNum].currencies[0].code;
-       }
-   } 
+//            randomNum = Math.floor(Math.random() * Math.floor(250));
+
+//            name = responseData[randomNum];
+      
+//            //use DOM manipulation to display name in html
+
+//       cityResults.innerHTML =
+//           "Catch up with " + name + "over lunch this week!";
+//        };
+//    };
+//    xhr.open('GET',url,true);
+//    xhr.send();
 }
