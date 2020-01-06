7303611ac3fc9cd2c996b2cf347745cc4014d9aa
corinnachan <corinnaccm@gmail.com>
2020-01-06 22:32:55 +0000

    reversed to original xmlhttprequest

--- getname.js
+++ getname.js
@@ -5,28 +5,28 @@ var obj = require('../classname.json');
 generatorButton.addEventListener("click", getDestination);
 
 function getName() {
-   randomNum = Math.floor(Math.random() * Math.floor(332));
-   name = obj[randomNum];
+//    randomNum = Math.floor(Math.random() * Math.floor(332));
+//    name = obj[randomNum];
    
-   results.innerHTML =
-        "Catch up with " + name + "over lunch this week!";
+//    results.innerHTML =
+//         "Catch up with " + name + "over lunch this week!";
 
    
-//     var xhr = new XMLHttpRequest;
-//    xhr.onreadystatechange = function () {
-//        if (xhr.readyState == 4 && xhr.status == 200) {
-//            var responseData = JSON.parse(xhr.response);
+    var xhr = new XMLHttpRequest;
+   xhr.onreadystatechange = function () {
+       if (xhr.readyState == 4 && xhr.status == 200) {
+           var responseData = JSON.parse(xhr.response);
 
-//            randomNum = Math.floor(Math.random() * Math.floor(250));
+           randomNum = Math.floor(Math.random() * Math.floor(250));
 
-//            name = responseData[randomNum];
+           name = responseData[randomNum];
       
-//            //use DOM manipulation to display name in html
+           //use DOM manipulation to display name in html
 
-//       cityResults.innerHTML =
-//           "Catch up with " + name + "over lunch this week!";
-//        };
-//    };
-//    xhr.open('GET',url,true);
-//    xhr.send();
+      cityResults.innerHTML =
+          "Catch up with " + name + "over lunch this week!";
+       };
+   };
+   xhr.open('GET',url,true);
+   xhr.send();
 }
